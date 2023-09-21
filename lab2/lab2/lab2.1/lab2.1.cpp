576ff3c5fa2c96729305c046dd302aee03c88abf
Ksenia <rastorguevak41@gmail.com>
2023-09-21 18:23:39 +0300

    Add lab2

---
+++ lab2/lab2/lab2.1/lab2.1.cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int* qs(int* items, int left, int right) { //вызов функции: qs(items, 0, count-1);
+    int i, j;
+    int x, y;
+
+    i = left; j = right;
+
+    /* выбор компаранда */
+    x = items[(left + right) / 2];
+
+    do {
+        while ((items[i] < x) && (i < right)) i++;
+        while ((x < items[j]) && (j > left)) j--;
+
+        if (i <= j) {
+            y = items[i];
+            items[i] = items[j];
+            items[j] = y;
+            i++; j--;
+        }
+    } while (i <= j);
+
+    if (left < j) qs(items, left, j);
+    if (i < right) qs(items, i, right);
+
+    return items;
+}
+
+
+#define N 50000
+int mas[N];
+int* masp = mas;
+int* otvet;
+
+int main() {
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        masp[i] = rand() % 100 - 20;
+        //printf(" %d ", masp[i]);
+    }
+
+    clock_t start = clock();
+    otvet = qs(masp, 0, N-1);
+    clock_t end = clock();
+
+    /*printf("\n\n");
+    for (int i = 0; i < N; i++) {
+        printf(" %d ", otvet[i]);
+    }*/
+
+    printf("\n\n");
+    printf("%f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    return 0;
+}
---
+++ lab2/lab2_2/teast_qsort/teast_qsort.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+char mas[5] = { 'г', 'а', 'в', 'Б', 'ё'};
+
+int compare(const void* i, const void* j) {
+    return *(char*)i - *(char*)j;
+}
+
+
+int main() {  
+    setlocale(0, "rus");
+    qsort(mas, 5, sizeof(char), compare);
+
+    for (int i = 0; i < 5; i++) {
+        printf("%c", mas[i]);
+    }
+   
+    return 0;
+
+
+}
