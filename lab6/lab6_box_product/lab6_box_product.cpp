5af4b904aa9c3556e524f5b41bb1ac34a5ba1caf
Ksenia <rastorguevak41@gmail.com>
2023-10-27 16:38:57 +0300

    Add lab6

---
+++ lab6/lab6_box_product/lab6_box_product.cpp
@@ -0,0 +1,174 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+	int n1, n2;
+	int v1_keyboard, v2_keyboard, v_tmp, v1, v2;
+	int size_G = 0;
+	//int col_one;
+
+	setlocale(0, "rus");
+	printf("Введите размер матрицы М1 -> ");
+	scanf("%d", &n1);
+	printf("Введите размер матрицы М2 -> ");
+	scanf("%d", &n2);
+	printf("\n");
+
+
+	int** G1 = (int**)malloc(n1 * sizeof(int*));
+	for (int i = 0; i < n1; i++) {
+		G1[i] = (int*)malloc(n1 * sizeof(int));
+	}
+
+	int** G2 = (int**)malloc(n2 * sizeof(int*));
+	for (int i = 0; i < n2; i++) {
+		G2[i] = (int*)malloc(n2 * sizeof(int));
+	}
+
+	printf("Матрица М1:\n");
+	srand(time(0));
+	for (int i = 0; i < n1; i++) {				//формирование матрицы смежности G1
+		for (int j = 0; j < n1; j++) {
+			if (j >= i) {							//неориентированный граф
+				if (i == j) G1[i][j] = 0;
+				else {
+					G1[i][j] = rand() % 2;
+					G1[j][i] = G1[i][j];
+				}
+			}
+			/*if (i == j) G1[i][j] = 0;				//ориентированный граф
+			else  G1[i][j] = rand() % 2;*/
+			printf(" %d", G1[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\nМатрица М2:\n");
+	for (int i = 0; i < n2; i++) {				//формирование матрицы смежности G2
+		for (int j = 0; j < n2; j++) {
+			if (j >= i) {							//неориентированный граф
+				if (i == j) G2[i][j] = 0;
+				else {
+					G2[i][j] = rand() % 2;
+					G2[j][i] = G2[i][j];
+				}
+			}
+			/*if (i == j) G1[i][j] = 0;				//ориентированный граф
+			else  G1[i][j] = rand() % 2;*/
+			printf(" %d", G2[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	int** E1 = (int**)malloc(n1 * sizeof(int*));
+	for (int i = 0; i < n1; i++) {
+		E1[i] = (int*)malloc(n1 * sizeof(int));
+	}
+
+	int** E2 = (int**)malloc(n2 * sizeof(int*));
+	for (int i = 0; i < n2; i++) {
+		E2[i] = (int*)malloc(n2 * sizeof(int));
+	}
+
+	for (int i = 0; i < n1; i++) {				//формирование единичной матрицы E1 для G1
+		for (int j = 0; j < n1; j++) {
+			if (i == j) E1[i][j] = 1;
+			else  E1[i][j] = 0;
+			//printf(" %d", E1[i][j]);
+		}
+	}
+
+
+	for (int i = 0; i < n2; i++) {				//формирование единичной матрицы E2 для G2
+		for (int j = 0; j < n2; j++) {
+			if (i == j) E2[i][j] = 1;
+			else  E2[i][j] = 0;
+			//printf(" %d", E2[i][j]);
+		}
+	}
+
+
+
+	int** Rez1 = (int**)malloc((n1*n2) * sizeof(int*));
+	for (int i = 0; i < (n1 * n2); i++) {
+		Rez1[i] = (int*)malloc((n1 * n2) * sizeof(int));
+	}
+
+	int** Rez2 = (int**)malloc((n1 * n2) * sizeof(int*));
+	for (int i = 0; i < (n1 * n2); i++) {
+		Rez2[i] = (int*)malloc((n1 * n2) * sizeof(int));
+	}
+
+	int j_r = 0;
+	int i_r = 0;
+
+	int tab = 0;
+	int flag_first = 1;
+	
+	for (int j_G1 = 0; j_G1 < n1; j_G1++) {											//транзисторное произведение А1 и Е2
+		i_r = 0;
+		for (int i_G1 = 0; i_G1 < n1; i_G1++) {
+			for (int i_E = 0; i_E < n2; i_E++) {
+				for (int j_E = 0; j_E < n2; j_E++) {
+					j_r = j_E + tab;		
+					Rez1[i_r][j_r] = G1[i_G1][j_G1] * E2[i_E][j_E];
+					//printf("[%d][%d] = %d\n", i_r, j_r, Rez[i_r][j_r]);
+					j_r++;
+				}
+
+				j_r = 0;
+				i_r++;
+			}
+		}
+		flag_first = 0;
+		tab = tab + n2;
+	}
+	
+
+	j_r = 0;
+	i_r = 0;
+	tab = 0;
+	flag_first = 1;
+	for (int j_E = 0; j_E < n1; j_E++) {											//транзисторное произведение Е1 и А2 
+		i_r = 0;
+		for (int i_E = 0; i_E < n1; i_E++) {
+			for (int i_G2 = 0; i_G2 < n2; i_G2++) {
+				for (int j_G2 = 0; j_G2 < n2; j_G2++) {
+					j_r = j_G2 + tab;
+					Rez2[i_r][j_r] = G2[i_G2][j_G2] * E1[i_E][j_E];
+					//printf("[%d][%d] = %d\n", i_r, j_r, Rez2[i_r][j_r]);
+					j_r++;
+				}
+
+				j_r = 0;
+				i_r++;
+			}
+		}
+		flag_first = 0;
+		tab = tab + n2;
+	}
+
+	
+	int** Box_product = (int**)malloc((n1 * n2) * sizeof(int*));					//А1*E2 + E1*A2
+	for (int i = 0; i < (n1 * n2); i++) {
+		Box_product[i] = (int*)malloc((n1 * n2) * sizeof(int));
+	}
+
+
+	printf("\nДекартово произведение М1 на М2:\n");
+	for (int i = 0; i < (n1 * n2); i++) {
+		for (int j = 0; j < (n1 * n2); j++) {
+			Box_product[i][j] = Rez1[i][j] + Rez2[i][j];
+			printf(" %d", Box_product[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ lab6/lab6_list/lab6_list.cpp
@@ -0,0 +1,190 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+	int n1, n2;
+	int v1_keyboard, v2_keyboard, v_tmp, v1, v2;
+	int size_G = 0;
+	//int col_one;
+
+	setlocale(0, "rus");
+	printf("Введите размер матрицы М1 -> ");
+	scanf("%d", &n1);
+	printf("Введите размер матрицы М2 -> ");
+	scanf("%d", &n2);
+	printf("\n");
+
+
+	int** G1 = (int**)malloc(n1 * sizeof(int*));
+	for (int i = 0; i < n1; i++) {
+		G1[i] = (int*)malloc(n1 * sizeof(int));
+	}
+
+	int** G2 = (int**)malloc(n2 * sizeof(int*));
+	for (int i = 0; i < n2; i++) {
+		G2[i] = (int*)malloc(n2 * sizeof(int));
+	}
+
+	srand(time(0));
+	for (int i = 0; i < n1; i++) {				//формирование матрицы смежности М1
+		for (int j = 0; j < n1; j++) {
+			if (i == j) G1[i][j] = 0;
+			else  G1[i][j] = rand() % 2;
+			printf(" %d", G1[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n\n");
+
+	for (int i = 0; i < n2; i++) {				//формирование матрицы смежности М2
+		for (int j = 0; j < n2; j++) {
+			if (i == j) G2[i][j] = 0;
+			else  G2[i][j] = rand() % 2;
+			printf(" %d", G2[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+
+	//отождествление вершин
+	printf("\n\nОтождествление вершин матрицы М1\n");
+	printf("Введите вершину 1 -> ");
+	scanf("%d", &v1_keyboard);
+	printf("Введите размер матрицы М2 -> ");
+	scanf("%d", &v2_keyboard);
+	printf("\n");
+
+	int** G1_otogd = (int**)malloc((n1 - 1) * sizeof(int*));
+	for (int i = 0; i < (n1 - 1); i++) {
+		G1_otogd[i] = (int*)malloc((n1 - 1) * sizeof(int));
+	}
+
+	if (v1_keyboard > v2_keyboard) {
+		v_tmp = v1_keyboard;
+		v1_keyboard = v2_keyboard;
+		v2_keyboard = v_tmp;
+	}
+
+
+	v1_keyboard--;
+	v2_keyboard--;
+
+	for (int i = 0; i < n1; i++) {				//"ИЛИ" для строки
+		if (i == v1_keyboard) {
+			for (int j = 0; j < n1; j++) {
+				G1[v1_keyboard][j] = G1[v1_keyboard][j] || G1[v2_keyboard][j];
+				G1[v2_keyboard][j] = 0;
+			}
+		}
+	}
+
+	for (int j = 0; j < n1; j++) {				//"ИЛИ" для столбеца
+		if (j == v1_keyboard) {
+			for (int i = 0; i < n1; i++) {
+				G1[i][v1_keyboard] = G1[i][v1_keyboard] || G1[i][v2_keyboard];
+				G1[i][v2_keyboard] = 0;
+			}
+		}
+	}
+
+	int i_otogd = 0;							//отождествление вершин + вывод результата
+	int j_otogd = 0;
+
+	for (int i = 0; i < n1; i++) {
+		if (i != v2_keyboard) {
+			for (int j = 0; j < n1; j++) {
+				if (j != v2_keyboard) {
+					G1_otogd[i_otogd][j_otogd] = G1[i][j];
+					printf(" %d", G1_otogd[i_otogd][j_otogd]);
+					j_otogd++;
+				}
+			}
+			i_otogd++;
+			printf("\n");
+		}
+
+	}
+
+
+
+
+
+
+	//стягивание ребра
+	printf("\n\nСтягивание ребра матрицы М1\n");
+	printf("Введите вершину 1 -> ");
+	scanf("%d", &v1_keyboard);
+	printf("Введите вершину 2 -> ");
+	scanf("%d", &v2_keyboard);
+	printf("\n");
+
+	for (int i = 0; i < n1; i++) {
+		for (int j = 0; j < n1; j++) {
+			if (i == v1_keyboard - 1) v1 = i;
+			if (j == v2_keyboard - 1) v2 = j;
+		}
+	}
+
+	G1[v1][v2] = 0;
+	G1[v2][v1] = 0;
+
+	for (int i = 0; i < n1; i++) {				//печать матрицы
+		for (int j = 0; j < n1; j++) {
+			printf(" %d", G1[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+	//расщепления вершины 
+	printf("\n\nРасщепление вершины матрицы М1\n");
+	printf("Введите вершину -> ");
+	scanf("%d", &v1_keyboard);
+
+
+
+
+	//меню
+	int menu_answer;
+	printf("1 - Формирование матрицы\n");
+	printf("2 - Отождествление вершины матрицы\n");
+	printf("3 - Стягивание ребра матрицы\n");
+	printf("4 - Расщепление вершины матрицы\n");
+	printf("0 - Выход\n");
+
+
+	while (1) {
+		printf("Введите выбранный вариант -> ");
+		scanf("%d", menu_answer);
+
+		switch (menu_answer) {
+		case(1): {
+
+		}
+
+		case(2): {
+
+		}
+
+		case(3): {
+
+		}
+
+		case(0): {
+			break;
+		}
+		}
+	}
+
+	return 0;
+}
+
